31da62ed124b6a721651be053b6bb5c910914d89
funduck007 <stepanoskin007@gmail.com>
2019-07-02 04:13:21 -0400

    plot_regplot: add alpha to scatter parameters

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -1,7 +1,8 @@
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_regplot(df, x, y, name=None):
+def plot_regplot(df, x, y, name=None, alpha=1):
     """
     plot a regression plot for variables x and y
     :param df: pandas DataFrame
@@ -12,9 +13,13 @@ def plot_regplot(df, x, y, name=None):
         name of the column containing the 2st variable
     :param name: string
         name to display at the top of the plot
+    :param alpha: float
+        transparency coefficient for the scatter plot
     :return: None
     """
-    ax = sns.regplot(data=df, x=x, y=y)
+    f, ax = plt.subplots(1)
+    sns.regplot(data=df, x=x, y=y, ax=ax, scatter_kws={'alpha': alpha})
     ax.set_title(name + ' dataset'
                  '\nrelationship between ' +
                  x + ' and ' + y)
+    plt.show()
