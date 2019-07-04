78902525bbba77e7505284bd16ccabf12fdca63b
funduck007 <stepanoskin007@gmail.com>
2019-07-04 02:59:29 -0400

    add plot_decision_regions

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
+from matplotlib.colors import ListedColormap
 
 
 def plot_regplot(df, x, y, name=None, alpha=1, order=1, color='blue',
@@ -41,3 +43,39 @@ def plot_regplot(df, x, y, name=None, alpha=1, order=1, color='blue',
                      x + ' and ' + y)
     if show_plot:
         plt.show()
+
+
+def plot_decision_regions(df, xcol1, xcol2, ycol, classifier, test_idx=None,
+                          resolution=0.02, alpha=0.5):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = df[xcol1].min() - 1, df[xcol1].max() + 1
+    x2_min, x2_max = df[xcol2].min() - 1, df[xcol2].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(df[ycol])):
+        mask = df[ycol] == cl
+        plt.scatter(x=df.loc[mask, xcol1], y=df.loc[mask, xcol2],
+                    alpha=alpha, c=colors[idx],
+                    marker=markers[idx], label=cl,
+                    edgecolor='black')
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0], X_test[:, 1],
+                    c='', edgecolor='black', alpha=1.0,
+                    linewidth=1, marker='o',
+                    s=100, label='test set')
