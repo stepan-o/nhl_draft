bdf632a6dd17931e9bee3e626a4d8457bdab66cc
funduck007 <stepanoskin007@gmail.com>
2019-07-04 23:57:30 -0400

    fit_class: add train-test split, plot resolution parameter, unfinished decision tree plot

--- src/class_utils.py
+++ src/class_utils.py
@@ -1,21 +1,51 @@
 from time import time
 
+import numpy as np
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression, Perceptron
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 from src.plot_utils import plot_decision_regions
 
 
-def fit_class(X, y, classifier='lr', xlabel="x1", ylabel="y2",
+def fit_class(X, y, test_size=0.3, stratify_y=True, scale=None,
+              classifier='lr', xlabel="x1", ylabel="y2",
               lr_c=100.0, perc_max_iter=40, perc_eta=0.1,
               svm_l_c=1.0, svm_k_c=1.0, svm_k_gamma=0.2,
               tree_criterion='gini', tree_max_depth=4,
               forest_criterion='gini', forest_n_estimators=25, forest_njobs=2,
               knn_nn=5, knn_p=2, knn_metric='minkowski',
-              random_state=1, plot_res='show', save_path=""):
+              random_state=1, plot_result='show', plot_resolution=0.02, save_path=""):
+    if stratify_y:
+        stratify = y
+    else:
+        stratify = None
+    print("----- Fitting classification algorithms to predict", y.name, "from", xlabel, ylabel,
+          "\nTotal samples in the dataset: {0:,}".format(len(X)))
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=0.3, random_state=random_state, stratify=stratify)
+    print('Labels counts in y_train:', np.bincount(y_train),
+          '\nLabels counts in y_test:', np.bincount(y_test),
+          '\nLabels counts in y:', np.bincount(y))
+
+    if scale:
+        if scale == 'norm':
+            scaler = MinMaxScaler(feature_range=(0, 1))
+        elif scale == 'std':
+            scaler = StandardScaler()
+        else:
+            raise AttributeError("Parameter 'scale' must be set to either 'norm' or 'std'.")
+        scaler.fit(X_train)
+        X = pd.DataFrame(scaler.transform(X))
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
+        print("\n --- Features scaled using StandardScaler.")
+
     models = {
         'lr': LogisticRegression(C=lr_c, random_state=random_state),
         'perc': Perceptron(max_iter=perc_max_iter, eta0=perc_eta, random_state=random_state),
@@ -37,8 +67,9 @@ def fit_class(X, y, classifier='lr', xlabel="x1", ylabel="y2",
             print("\n----- Fitting", name.upper())
             t = time()
             model.fit(X, y)
-            plot_decision_regions(X, y, classifier=model, alpha=0.3, res=plot_res, name=name,
+            plot_decision_regions(X, y, classifier=model, alpha=0.3, result=plot_result, name=name,
                                   xlabel=xlabel, ylabel=ylabel, save_path=save_path,
+                                  resolution=plot_resolution,
                                   title=name.upper() + " classification algorithm, "
                                                        "\ndecision boundary"
                                                        "\nx1: {0}, x2: {1}".format(xlabel, ylabel))
@@ -54,8 +85,9 @@ def fit_class(X, y, classifier='lr', xlabel="x1", ylabel="y2",
             print("\n----- Fitting", classi.upper())
             t = time()
             model.fit(X, y)
-            plot_decision_regions(X, y, classifier=model, alpha=0.3, res=plot_res, name=classi,
+            plot_decision_regions(X, y, classifier=model, alpha=0.3, result=plot_result, name=classi,
                                   xlabel=xlabel, ylabel=ylabel, save_path=save_path,
+                                  resolution=plot_resolution,
                                   title=classi.upper() + " classification algorithm, "
                                                          "\ndecision boundary"
                                                          "\nx1: {0}, x2: {1}".format(xlabel, ylabel))
@@ -70,10 +102,23 @@ def fit_class(X, y, classifier='lr', xlabel="x1", ylabel="y2",
         print("\n----- Fitting", classifier.upper())
         t = time()
         model.fit(X, y)
-        plot_decision_regions(X, y, classifier=model, alpha=0.3, res=plot_res, name=classifier,
+        plot_decision_regions(X, y, classifier=model, alpha=0.3, result=plot_result, name=classifier,
                               xlabel=xlabel, ylabel=ylabel, save_path=save_path,
+                              resolution=plot_resolution,
                               title=classifier.upper() + " classification algorithm, "
                                                          "\ndecision boundary"
                                                          "\nx1: {0}, x2: {1}".format(xlabel, ylabel))
         elapsed = time() - t
         print("\n - took {0:.2f} seconds.".format(elapsed))
+
+#
+# dot_data = export_graphviz(model,
+#                           filled=True,
+#                           rounded=True,
+#                           class_names=['def',
+#                                        'for'],
+#                           feature_names=[xcol1,
+#                                          xcol2],
+#                           out_file=None)
+# graph = graph_from_dot_data(dot_data)
+# graph.write_png('img/tree.png')
